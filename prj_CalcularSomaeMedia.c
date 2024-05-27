9e6faab3ac15a2d391c755e50200741a0169bba8
Pedro Afonso Machado Vasconcelos <pedroafonsomachado116@gmail.com>
2024-05-27 11:18:10 -0300

    Create prj_CalcularSomaeMedia.c

---
+++ prj_CalcularSomaeMedia.c
@@ -0,0 +1,12 @@
+void CalcularSomaeMedia(){
+		int numero = 0;
+		int soma = 0;
+			printf("digite 10 números inteiros:\n");
+		for (int i = 0; i < 10; i++){
+			scanf("%d", &numero );
+				soma += numero;
+		double media = soma/10;
+			printf("A soma é:%d\n", soma);
+			printf("A média é:%.2lf\n", media);
+	}
+}
