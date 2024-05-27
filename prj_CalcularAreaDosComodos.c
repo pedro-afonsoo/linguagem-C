52c70173eb355bbf2caadeda220509bab16610cb
Pedro Afonso Machado Vasconcelos <pedroafonsomachado116@gmail.com>
2024-05-27 11:18:50 -0300

    Create prj_CalcularAreaDosComodos.c

---
+++ prj_CalcularAreaDosComodos.c
@@ -0,0 +1,16 @@
+void CalcularAreaDosComodos(){
+		double largura, comprimento, area, areatotal;
+		for (int i = 1; i <= 4; i++){
+			printf("Digite a largura do cômodo:");
+			scanf("%lf", &largura);
+			printf("Digite o comprimento do cômodo:");
+			scanf("%lf", &comprimento);
+
+			area = largura * comprimento;
+			areatotal += area;
+
+			printf("A área do cômodo %d é:%.2lf unidades de área\n", i, area);
+		}
+			printf("A área total é:%.2lf", areatotal);
+			//area total aprecerá após terminar o loop
+}
