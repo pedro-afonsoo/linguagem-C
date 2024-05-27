1e1f9a114e9924d16ea9820ec9577d9fb5d60d62
Pedro Afonso Machado Vasconcelos <pedroafonsomachado116@gmail.com>
2024-05-27 11:20:52 -0300

    Create prj_DinheiroFumante.c

---
+++ prj_DinheiroFumante.c
@@ -0,0 +1,17 @@
+void DinheiroFumante() {
+    	double maço, medio, ano, cigarro, dinheiro;
+
+   	 	printf("Quantos maços você fuma por dia?\n ");
+    	 	scanf("%lf", &maço);
+
+   		 printf("Qual o preço médio de um maço?\n ");
+   	 	scanf("%lf", &medio);
+
+    		printf("Há quantos anos você fuma?\n ");
+    		scanf("%lf", &ano);
+
+    		cigarro = maço * 20 * ano * 365;
+    		dinheiro = cigarro * medio / 20;
+
+    		printf("Você fumou no total %.0lf cigarros e gastou %.2lf em %.0lf anos", cigarro, dinheiro, ano);
+}
